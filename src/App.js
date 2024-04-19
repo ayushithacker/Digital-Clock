5fafbfebb58ac7ea12492d7cbf04925e4fad22aa
Ayushi Thacker <ayushi.thacker@pc0598.radixweb.local>
2024-04-19 12:18:55 +0530

    clock

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-
+import {useState} from "react"
 function App() {
+
+
+  const [time, setTime] = useState(0)
+
+
+
+  const  clock  = ()=>{
+    let t = new Date()
+    setTime(t.toLocaleTimeString());
+  }
+  
+
+  setInterval(clock,1000)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+      <h1>Digital clock</h1>
+      <p>{time}</p>
+
+    
     </div>
   );
 }
